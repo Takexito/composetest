c493c781317214ae4eb097b205034757de2403b3
Andrey <andrey.yavorskii@dev.mobileup.ru>
2022-04-29 15:37:08 +0300

    integrate coin module to app module

--- app/src/main/kotlin/ru/mobileup/template/DI.kt
+++ app/src/main/kotlin/ru/mobileup/template/DI.kt
@@ -1,9 +1,11 @@
 package ru.mobileup.template
 
 import ru.mobileup.core.coreModule
+import ru.mobileup.features.coins.coinModule
 import ru.mobileup.features.pokemons.pokemonsModule
 
 val allModules = listOf(
     coreModule(BuildConfig.BACKEND_URL),
-    pokemonsModule
+    pokemonsModule,
+    coinModule
 )
