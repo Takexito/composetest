8f5a6322ac15a82884e8f67f02bfa782e0783fc7
Andrey <andrey.yavorskii@dev.mobileup.ru>
2022-05-12 14:17:30 +0300

    add TopAppBar.kt

---
+++ features/src/main/kotlin/ru/mobileup/features/collection/ui/TopAppBar.kt
@@ -0,0 +1,80 @@
+package ru.mobileup.features.collection.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CollectionTopAppBar(
+    modifier: Modifier = Modifier,
+    buttons: List<TopBarButtonConfig>,
+    buttonClick: (buttonId: String) -> Unit,
+) {
+    TopAppBar(
+        modifier = modifier,
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    ) {
+        TopBarContentButtons(
+            modifier = Modifier.padding(16.dp),
+            buttons = buttons,
+            buttonClick = buttonClick
+        )
+    }
+}
+
+@Composable
+fun TopBarContentButtons(
+    modifier: Modifier = Modifier,
+    buttons: List<TopBarButtonConfig> = emptyList(),
+    buttonClick: (buttonId: String) -> Unit,
+) {
+    Row(
+        modifier
+            .fillMaxSize()
+            .background(Color.Transparent),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        buttons.forEach { config ->
+            TopBarButton(
+                config = config,
+                onClick = buttonClick
+            )
+        }
+    }
+}
+
+@Composable
+fun TopBarButton(
+    modifier: Modifier = Modifier,
+    config: TopBarButtonConfig,
+    onClick: (buttonId: String) -> Unit
+) {
+    IconButton(
+        modifier = modifier
+            .fillMaxHeight()
+            .background(Color.Transparent),
+        onClick = { onClick(config.id) },
+    ) {
+        Icon(
+            modifier = Modifier.size(config.iconSize),
+            painter = config.iconPainter,
+            contentDescription = config.contentDescription
+        )
+    }
+}
+
+class TopBarButtonConfig(
+    val id: String,
+    val iconPainter: Painter,
+    val contentDescription: String = id,
+    val iconSize: Dp = 24.dp,
+)
