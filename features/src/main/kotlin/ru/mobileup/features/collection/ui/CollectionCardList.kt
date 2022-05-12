f781fccdf961c9114845c05a1cb6191c3963b915
Andrey <andrey.yavorskii@dev.mobileup.ru>
2022-05-12 14:17:44 +0300

    add CollectionHeader.kt

---
+++ features/src/main/kotlin/ru/mobileup/features/collection/ui/CollectionCardList.kt
@@ -0,0 +1,111 @@
+package ru.mobileup.features.collection.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.mobileup.core.R
+import ru.mobileup.core.theme.CollectionHeaderShape
+
+typealias CollectionItemType = Any
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun CollectionCardList(
+    modifier: Modifier = Modifier,
+    cardOverlapSize: Dp = (-64).dp,
+    collections: List<CollectionItemType>,
+    onItemClick: (item: CollectionItemType, index: Int) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(cardOverlapSize)
+    ) {
+        collections.forEachIndexed { index, collection ->
+            CollectionCardItem(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                cardConfig = CollectionCardItemCardConfig(
+                    backgroundPainter = painterResource(id = R.drawable.ic_launcher_background)
+                ),
+                titleConfig = CollectionCardItemTitleConfig(
+                    title = "Title $index",
+                ),
+                onClick = { onItemClick(collection, index) }
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun CollectionCardItem(
+    modifier: Modifier,
+    cardConfig: CollectionCardItemCardConfig = CollectionCardItemCardConfig(
+        painterResource(
+            id = R.drawable.ic_launcher_background
+        )
+    ),
+    titleConfig: CollectionCardItemTitleConfig = CollectionCardItemTitleConfig(title = "Animal"),
+    onClick: () -> Unit,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(cardConfig.aspectRatio),
+        onClick = onClick,
+        shape = cardConfig.shape,
+        border = cardConfig.border,
+        elevation = 4.dp,
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = titleConfig.titleAlignment,
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                painter = cardConfig.backgroundPainter,
+                contentDescription = cardConfig.contentDescription
+            )
+            Text(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(titleConfig.titlePaddingValues),
+                fontSize = titleConfig.titleFontSize,
+                text = titleConfig.title,
+            )
+        }
+    }
+}
+
+class CollectionCardItemTitleConfig(
+    val title: String,
+    val titleAlignment: Alignment = Alignment.TopStart,
+    val titlePaddingValues: PaddingValues = PaddingValues(start = 32.dp, top = 32.dp),
+    val titleFontSize: TextUnit = 24.sp
+)
+
+class CollectionCardItemCardConfig(
+    val backgroundPainter: Painter,
+    val contentDescription: String = "",
+    val aspectRatio: Float = 3f / 2f,
+    val shape: Shape = CollectionHeaderShape.medium,
+    val border: BorderStroke = BorderStroke(1.dp, Color.Gray),
+)
---
+++ features/src/main/kotlin/ru/mobileup/features/collection/ui/CollectionHeader.kt
@@ -0,0 +1,119 @@
+package ru.mobileup.features.collection.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.mobileup.core.theme.CollectionHeaderShape
+import ru.mobileup.features.R
+
+typealias HeaderStatistic = Any
+
+@Composable
+fun CollectionHeader(
+    modifier: Modifier = Modifier,
+    shape: Shape = CollectionHeaderShape.medium,
+    backgroundColor: Color = Color.Transparent,
+    border: BorderStroke = BorderStroke(1.dp, Color.LightGray),
+    separateBoarder: BorderStroke = BorderStroke(0.5.dp, Color.LightGray), // TODO: rename
+    contentPaddings: PaddingValues = PaddingValues(vertical = 16.dp),
+    statistics: List<HeaderStatistic>,
+    onItemClick: (pos: Int) -> Unit,
+) {
+    Surface(
+        modifier = modifier,
+        shape = shape,
+        color = backgroundColor,
+        border = border
+    ) {
+        Row(
+            modifier = Modifier,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            statistics.forEachIndexed { index, statistic ->
+                CollectionHeaderItem(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                    iconConfig = CollectionHeaderItemIconConfig(
+                        iconPainter = painterResource(id = R.drawable.ic_baseline_attach_money_24),
+                    ),
+                    textConfig = CollectionHeaderItemTextConfig(text = (1000 * index).toString()),
+                    backgroundColor = backgroundColor,
+                    border = separateBoarder,
+                    contentPaddings = contentPaddings,
+                    spaceBetweenIconAndText = 16.dp,
+                    onClick = { onItemClick(index) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CollectionHeaderItem(
+    modifier: Modifier,
+    backgroundColor: Color,
+    border: BorderStroke,
+    contentPaddings: PaddingValues,
+    spaceBetweenIconAndText: Dp,
+    textConfig: CollectionHeaderItemTextConfig,
+    iconConfig: CollectionHeaderItemIconConfig,
+    onClick: () -> Unit,
+) {
+    Surface(
+        modifier = modifier
+            .clickable { onClick() },
+        color = backgroundColor,
+        border = border
+    ) {
+        Column(
+            modifier = Modifier.padding(contentPaddings),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                modifier = Modifier
+                    .height(iconConfig.iconSize)
+                    .fillMaxWidth(),
+                painter = iconConfig.iconPainter,
+                contentDescription = iconConfig.contentDescription
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(spaceBetweenIconAndText)
+                    .fillMaxWidth()
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = textConfig.text,
+                textAlign = TextAlign.Center,
+                fontSize = textConfig.fontSize
+            )
+        }
+    }
+}
+
+class CollectionHeaderItemTextConfig(
+    val text: String,
+    val fontSize: TextUnit = 20.sp,
+)
+
+class CollectionHeaderItemIconConfig(
+    val iconPainter: Painter,
+    val iconSize: Dp = 32.dp,
+    val contentDescription: String = "",
+)
